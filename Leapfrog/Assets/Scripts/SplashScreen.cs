5f7ee0af1875b8c17f259f034adf58e4384ea0b4
kg718 <kggoodall18@gmail.com>
2023-05-16 11:04:21 +0100

    Splash Sceen
    It Work but bad

--- Leapfrog/Assets/Scripts/SplashScreen.cs
+++ Leapfrog/Assets/Scripts/SplashScreen.cs
@@ -3,16 +3,20 @@ using UnityEngine.SceneManagement;
 
 public class SplashScreen : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    public float SplashTimer;
+    private float CurrentSplashTime;
     // Update is called once per frame
     void Update()
     {
         if(Input.touchCount > 0)
+        {
+            SceneManager.LoadScene(1);
+        }
+        if(CurrentSplashTime < SplashTimer)
+        {
+            CurrentSplashTime += Time.deltaTime;
+        }
+        else
         {
             SceneManager.LoadScene(1);
         }
